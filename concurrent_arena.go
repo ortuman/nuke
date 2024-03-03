5a52985b74c0589c3bd6ed4555615ed074686761
Miguel Ángel Ortuño <ortuman@gmail.com>
2024-03-03 11:23:31 +0100

    new concurrent arena implementation
    Signed-off-by: Miguel Ángel Ortuño <ortuman@gmail.com>

---
+++ concurrent_arena.go
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package nuke
+
+import (
+	"sync"
+	"unsafe"
+)
+
+type concurrentArena struct {
+	mtx sync.Mutex
+	a   Arena
+}
+
+// NewConcurrentArena returns an arena that is safe to be accessed concurrently
+// from multiple goroutines.
+func NewConcurrentArena(a Arena) Arena {
+	return &concurrentArena{a: a}
+}
+
+// Alloc satisfies the Arena interface.
+func (a *concurrentArena) Alloc(size int) unsafe.Pointer {
+	a.mtx.Lock()
+	ptr := a.a.Alloc(size)
+	a.mtx.Unlock()
+	return ptr
+}
+
+// Reset satisfies the Arena interface.
+func (a *concurrentArena) Reset(release bool) {
+	a.mtx.Lock()
+	a.a.Reset(release)
+	a.mtx.Unlock()
+}
